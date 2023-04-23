7cbc68acb08652371fba4f8eb93eebf274ce6a43
devenseth <devenseth8@gmail.com>
2023-04-23 17:18:15 -0400

    project

--- src/File_Away.java
+++ src/File_Away.java
@@ -1,18 +1,12 @@
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import static java.lang.System.out;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.CREATE;
 import java.util.ArrayList;
 import javax.swing.JFileChooser;
@@ -22,74 +16,58 @@ public class File_Away {
 
     public static void main(String[] args) {
         JFileChooser chooser = new JFileChooser(); // sets up file chooser.
-        File selectedFile;
-        String rec = "";
+        //var declarations
+        File selectedFile; // var to hold the users selected file
+        String rec = ""; // stores the file text
         ArrayList<String> lines = new ArrayList<>(); // array which holds file lines
-        String [] wordsNum = new String[] {};
-        int charNum = 0;
+        String [] wordsNum = new String[] {}; // var used to find the number of words
+        int charNum = 0; // number of characters
         int wordCount = 0;//number of words
 
         try
         {
-           File workingDirectory = new File(System.getProperty("user.dir"));
+           File workingDirectory = new File(System.getProperty("user.dir")); // gives current directory of user
 
-                       chooser.setCurrentDirectory(workingDirectory);
+                       chooser.setCurrentDirectory(workingDirectory); // gives user files to choose from
 
-            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) // if the user chooses an a file
             {
-                selectedFile = chooser.getSelectedFile();
+                selectedFile = chooser.getSelectedFile(); // sets var to the file user chooses
                 Path file = selectedFile.toPath();
-                // Typical java pattern of inherited classes
-                // we wrap a BufferedWriter around a lower level BufferedOutputStream
-                InputStream in =
-                        new BufferedInputStream(Files.newInputStream(file, CREATE));
-                BufferedReader reader =
-                        new BufferedReader(new InputStreamReader(in));
-                int line = 0;
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                int line = 0; // var to report line numbers
                 while(reader.ready())
                 {
-                    rec = reader.readLine();
-                    lines.add(rec);
-                     wordsNum = rec.split(" ");
-                     wordCount = wordCount + wordsNum.length;
-                     charNum = charNum + rec.length();
-                    line++;
-                    // echo to screen
-                    System.out.printf("\nLine %4d %-60s ", line, rec);
-                    System.out.println("\n");
+                    rec = reader.readLine(); // the string var reads the line
+                    lines.add(rec); // the text is stored in the var
+                     wordsNum = rec.split(" "); // counts number of words using split
+                     wordCount = wordCount + wordsNum.length; // stores number of words in int var
+                     charNum = charNum + rec.length(); // finds number of characters in file
+                    line++; // increments line number
+                    System.out.printf("\nLine %4d %-60s ", line, rec); // displays text file with line numbers
                 }
 
-
-
-                for(String l:lines)
-                {
-                    System.out.println(l);
-                }
-
-                String fields[] = lines.get(5).split(", ");
-                for(String f:fields)
-                    System.out.println(f);
-
-                reader.close(); // must close the file to seal it and flush buffer
-                System.out.println("\n\nData file read!");
+                reader.close(); // closes file
+                System.out.println("\n\nThe file was successfully read!"); // outputs message to user
                 System.out.println("The number of lines in the file are: " + line); // outputs the number of lines in file
-                System.out.println("The number of words in the data file are: " + wordCount);
-                System.out.println("The number of characters in the file are: " + charNum);
-                System.out.println("The name of the file read is: " + file.getFileName());
+                System.out.println("The number of words in the data file are: " + wordCount); // outputs number of words
+                System.out.println("The number of characters in the file are: " + charNum); // outputs number of characters
+                System.out.println("The name of the file read is: " + file.getFileName()); // outputs name of file
             }
-            else  // user closed the file dialog without choosing
+            else  // the user did not choose a file
             {
-                System.out.println("Failed to choose a file to process");
-                System.out.println("Run the program again!");
-                System.exit(0);
+                System.out.println("Failed to choose a file to process"); // outputs message to user
+                System.out.println("Run the program again!"); // outputs message to user
+                System.exit(0); // ends the program
             }
-        }  // end of TRY
-        catch (FileNotFoundException e)
+        }
+        catch (FileNotFoundException e) // possible error
         {
-            System.out.println("File not found!!!");
+            System.out.println("File not found!!!"); // outputs message if error occurs
             e.printStackTrace();
         }
-        catch (IOException e)
+        catch (IOException e) // possible error
         {
             e.printStackTrace();
         }
